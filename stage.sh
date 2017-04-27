30b381c87866177d9f69afd2b5a4137742359306
Jermolene <jeremy@jermolene.com>
2017-04-27 10:32:33 +0100

    Initial commit

---
+++ stage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ../jermolene.github.io/tiddlywiki-eu-meetup-2017
+
+rm -Rf ../jermolene.github.io/tiddlywiki-eu-meetup-2017/*
+
+cp -R wiki/output/* ../jermolene.github.io/tiddlywiki-eu-meetup-2017
